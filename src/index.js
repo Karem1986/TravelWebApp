af00194e235842f46574d932d098533828707018
krishnadevz <krishnakakade77@gmail.com>
2021-07-06 17:32:22 +0530

    navbar initial commits

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+// import "./index.css";
+import "./styles/tailwind.css";
+import Navbar from "./components/Navbar";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    {/*     <React.StrictMode>
+     */}
+    {/*    </React.StrictMode>
+     */}{" "}
+    <switch>
+    <Route exact path="/" component={Navbar} />
+
+    </switch>
+
+
+
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
