73f2eb5817dc4c1a8dd00bcb1f63092bc053ad68
krishnadevz <krishnakakade77@gmail.com>
2021-07-09 12:32:09 +0530

    section card conponent intial commits

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,25 +1,59 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import Weather from "./svg/weather";
+import Plane from "./svg/plane";
 export default class Category extends Component {
-    render() {
-        return (
-            <div className="mt-5 ">
-                <p className=" text-center text-gray-600">CATEGORY</p>
-                
-                <p className="text-center text-4xl font-bold">We Offer Best Services</p>
-                <div class="max-w-xl mx-auto bg-white rounded-xl shadow-md overflow-hidden ">
-        <div class="md:flex">
-          <div class="md:flex-shrink-0">
-            <img class="h-48 w-full object-cover md:w-48" src="https://images.unsplash.com/photo-1478098711619-5ab0b478d6e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=448&q=80" alt="A cat"/>
-          </div>
-          <div class="p-8">
-            <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Cat News</div>
-            <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Cats are people's best friends</a>
-            <p class="mt-2 text-gray-500">According to a study, it has been proven that there is a tight bond between cats and humans.</p>
-          </div>
-        </div>
-</div>
+  render() {
+    return (
+      <div className="mt-5 ">
+        <p className=" text-center text-gray-600">CATEGORY</p>
+
+        <p className="text-center text-4xl font-bold">We Offer Best Services</p>
+
+        <body class=" flex flex-wrap -mx-4 items-center h-screen">
+          
+          <div class="bg-white p-10 h-80 w-64 rounded-lg shadow-md">
+            <Weather />
+            <h1 class="text-lg font-bold py-4">Calculated Weather </h1>
+            <div class="mt-4 mb-10">
+              <p class="text-gray-600">
+                Built Wicket longer admire do barton vanity itself do in it.
+              </p>
+              <div class="bg-pink-400 w-3/4 h-full rounded-lg shadow-md"></div>
             </div>
-        )
-    }
+          </div>
+          <div class="bg-white p-10 h-80 w-64 rounded-lg shadow-md">
+            <Plane />
+            <h1 class="text-lg font-bold py-4">Calculated Weather </h1>
+            <div class="mt-4 mb-10">
+              <p class="text-gray-600">
+                Built Wicket longer admire do barton vanity itself do in it.
+              </p>
+              <div class="bg-pink-400 w-3/4 h-full rounded-lg shadow-md"></div>
+            </div>
+          </div>
+          <div class="bg-white p-10 h-80 w-64 rounded-lg shadow-md">
+            <Weather />
+            <h1 class="text-lg font-bold py-4">Calculated Weather </h1>
+            <div class="mt-4 mb-10">
+              <p class="text-gray-600">
+                Built Wicket longer admire do barton vanity itself do in it.
+              </p>
+              <div class="bg-pink-400 w-3/4 h-full rounded-lg shadow-md"></div>
+            </div>
+          </div>
+          <div class="bg-white p-10 h-80 w-64 rounded-lg shadow-md">
+            <Weather />
+            <h1 class="text-lg font-bold py-4">Calculated Weather </h1>
+            <div class="mt-4 mb-10">
+              <p class="text-gray-600">
+                Built Wicket longer admire do barton vanity itself do in it.
+              </p>
+              <div class="bg-pink-400 w-3/4 h-full rounded-lg shadow-md"></div>
+            </div>
+          </div>
+         
+        </body>
+      </div>
+    );
+  }
 }
