2851f67ededa38571bbd8f791fb656ab73de7ed2
krishnadevz <krishnakakade77@gmail.com>
2021-07-12 16:06:19 +0530

    book steps section commits

--- src/components/Category.js
+++ src/components/Category.js
@@ -9,10 +9,11 @@ export default class Category extends Component {
       <div className="mt-5 ">
         <p className=" text-center text-gray-600">CATEGORY</p>
 
-        <p className=" text-center text-4xl font-bold">We Offer Best Services</p>
+        <p className=" text-center text-4xl font-bold">
+          We Offer Best Services
+        </p>
 
         <body class=" flex justify-center   py-20  flex flex-wrap -mx-5  h-screen text-center  p-4 ">
-          
           <div class="bg-white p-10 h-80 w-64 rounded-lg bg-gray-100 shadow-md ml-0 mb-4 transform transition duration-500 hover:scale-110">
             <Weather />
             <h1 class="text-lg font-bold py-4">Calculated Weather </h1>
@@ -27,16 +28,17 @@ export default class Category extends Component {
             <h1 class="text-lg font-bold py-4">Best Flights </h1>
             <div class="mt-4 mb-10">
               <p class="text-gray-600">
-              Engrossed listening. Park gate sell they west hard for the.
+                Engrossed listening. Park gate sell they west hard for the.
               </p>
             </div>
           </div>
           <div class="bg-white p-10 h-80 w-64 rounded-lg bg-gray-100 shadow-md ml-6 mb-4 transform transition duration-500 hover:scale-110">
-            <img className="ml-7"src={Mic} alt="mic" width="92" height="78" />
+            <img className="ml-7" src={Mic} alt="mic" width="92" height="78" />
             <h1 class="text-lg font-bold py-4">Local Events </h1>
             <div class="mt-4 mb-10">
               <p class="text-gray-600">
-              Barton vanity itself do in it. Preferd to men it engrossed listening. 
+                Barton vanity itself do in it. Preferd to men it engrossed
+                listening.
               </p>
             </div>
           </div>
@@ -45,11 +47,10 @@ export default class Category extends Component {
             <h1 class="text-lg font-bold py-4">Customization </h1>
             <div class="mt-4 mb-10">
               <p class="text-gray-600">
-              We deliver outsourced aviation services for military customers
+                We deliver outsourced aviation services for military customers
               </p>
             </div>
           </div>
-         
         </body>
       </div>
     );
--- src/components/Steps.js
+++ src/components/Steps.js
@@ -1,12 +1,57 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Desti from "./svg/desti";
+import Payment from "./svg/payment";
+import Airport from "./svg/Airport";
+import Greece from   "../images/greece.jpg"
+import Petal from "./svg/petal";
+import Maps from "./svg/maps";
+import Map from "./svg/map";
+import Build from "./svg/build";
+import Heart from "./svg/heart";
 
 export default class Steps extends Component {
-    render() {
-        return (
-            <div className="ml-20 ">
-                <p className="font-bold text-gray-700" >Easy and Fast</p>
-                <p className="text-5xl font-bold">Book your next trip in 3 easy steps</p>
-            </div>
-        )
-    }
+  render() {
+    return (
+       <> 
+      <div className="ml-20 w-2/4 ">
+        <p className="font-bold text-gray-700">Easy and Fast</p>
+        <p className="text-5xl font-bold mt-2">
+          Book your next trip in 3 easy steps
+        </p>
+        <Desti />{" "}
+        <p className="text-base font-bold text-gray-500 ml-24 -mt-10 ">
+          Choose Destination
+        </p>
+        <p className="ml-24  font-bold text-gray-700 w-2/3">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna, tortor
+          tempus.{" "}
+        </p>
+        <Payment />{" "}
+        <p className="text-base font-bold text-gray-500 ml-24 -mt-10 ">
+          Make Payment
+        </p>
+        <p className="ml-24  font-bold text-gray-700 w-2/3">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna, tortor
+          tempus.{" "}
+        </p>
+        <Airport />{" "}
+        <p className="text-base font-bold text-gray-500 ml-24 -mt-10 ">
+          Make Payment
+        </p>
+        <p className="ml-24  font-bold text-gray-700 w-2/3">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna, tortor
+          tempus.{" "}
+        </p>
+        
+      </div>
+      <div class="bg-white  h-96 w-96 rounded-lg float-right bg-gray-100 shadow-md  mr-40 -mt-80 ">
+          <img className=" p-2 w-96 h-48 rounded-3xl " src={Greece} alt="Greece"/>
+          <p className="ml-4 font-bold ">Trip To Greece</p>
+          <p className="m-4 text-gray-700">14-29 June |   by Robbin joseph</p>
+          <p className="m-4 flex flex-row"><Petal/> <Maps/> <Map/> </p>
+         <Build/> <p className=" ml-11 -mt-5 text-gray-700">24 people going<Heart/></p>
+         </div>
+      </>
+    );
+  }
 }
